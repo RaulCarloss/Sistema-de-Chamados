a3a9af5cfe11470d2b99174b7a5a943bc7bf7aeb
Raul Carlos <raulcarlos96@hotmail.com>
2024-02-13 20:42:21 -0300

    pagina de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,45 @@
-
+import { useState } from 'react' 
+import { Link } from 'react-router-dom'; 
+import logo from '../../assets/logo.png'
 
 export default function SignUp(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
     return(
-        <div>
-            
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='logo do sistema de chamados' />
+                </div>
+
+                <form>
+                    <h1>Nova Conta</h1>
+                    <input 
+                        type='text'
+                        placeholder='Digite seu nome'
+                        value={name}
+                        onChange={ (e) => setName(e.target.value) }
+                    />
+                    <input 
+                        type='text'
+                        placeholder='Digite seu email'
+                        value={email}
+                        onChange={ (e) => setEmail(e.target.value) }
+                    />
+                    <input 
+                        type='password'
+                        placeholder='Digite sua senha'
+                        value={password}
+                        onChange={ (e) => setPassword(e.target.value) }
+                    />
+                    <button type='submit'>Cadastrar</button>
+                </form>
+
+                <Link to="/">Já possui uma conta? Faça login</Link>
+
+            </div> 
         </div>
     )
 }
