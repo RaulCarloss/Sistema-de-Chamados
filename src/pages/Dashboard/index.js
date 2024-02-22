7711b13315ffb236f73a7cfcb1d536e2674adbec
Raul Carlos <raulcarlos96@hotmail.com>
2024-02-21 23:41:36 -0300

    Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,11 @@
 import { useContext } from 'react'
 import { AuthContext } from '../../contexts/auth'
-import Header from '../../components/Header';
+import Header from '../../components/Header'
+import Title from '../../components/Title'
+import { FiPlus, FiMessageSquare } from 'react-icons/fi'
+import { Link } from 'react-router-dom' 
+
+import './dashboard.css'
 
 
 export default function Dashboard(){
@@ -14,8 +19,24 @@ export default function Dashboard(){
     <div>
       <Header/>
 
-      <h1>Pagina Dashboard</h1>
-      <button onClick={handleLogout}>Sair da conta</button>
+      <div className='content'> 
+        <Title name="Tickets">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        <>
+          <Link to="/new" className='new'>
+            <FiPlus color='#fff' size={25} />
+          </Link>
+
+          <div className='container'>
+
+          </div>
+        </>
+
+      </div>
+
+      
     </div>
   )
 }
