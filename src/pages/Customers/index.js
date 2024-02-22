919fbdf9d791a10b12ba6e0b47387c6e8620a3d1
Raul Carlos <raulcarlos96@hotmail.com>
2024-02-21 23:22:54 -0300

    Customers

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -2,14 +2,37 @@ import Header from '../../components/Header'
 import Title from '../../components/Title'
 import { FiUser } from 'react-icons/fi'
 import { useState } from 'react' 
+import { toast } from 'react-toastify' 
+import { db } from '../../services/firebaseConnection'
+import { addDoc, collection }  from 'firebase/firestore'
 
 export default function Customers(){
     const [nome, setNome] = useState('')
     const [cnpj, setCnpj] = useState('')
     const [endereco, setEndereco] = useState('')
 
-    function handleRegister(e){
+    async function handleRegister(e){
         e.preventDefault();
+
+        if(nome !== '' && cnpj !== '' && endereco !== ''){
+            await addDoc(collection(db, "customers"), {
+                nomeFantasia: nome,
+                cnpj: cnpj,
+                endereco: endereco
+            })
+            .then(() => {
+                setNome('')
+                setCnpj('')
+                setEndereco('')
+                toast.success("Empresa registrada!")
+            })
+            .catch((error) => {
+                console.log(error);
+                toast.error("Erro ao fazer o cadastro.")
+            })
+        } else{
+            toast.error("Preencha todos os campos!")
+        }
     }
 
 
