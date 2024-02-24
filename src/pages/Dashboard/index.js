b4e292a316515966b9f557471d6e53342d1d01af
Raul Carlos <raulcarlos96@hotmail.com>
2024-02-23 21:38:11 -0300

    dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import { AuthContext } from '../../contexts/auth'
 import Header from '../../components/Header'
 import Title from '../../components/Title'
-import { FiPlus, FiMessageSquare } from 'react-icons/fi'
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom' 
 
 import './dashboard.css'
@@ -29,9 +29,36 @@ export default function Dashboard(){
             <FiPlus color='#fff' size={25} />
           </Link>
 
-          <div className='container'>
+         <table>
 
-          </div>
+            <thead>
+                <tr>
+                  <th scope='col'>Cliente</th>
+                  <th scope='col'>Assunto</th>
+                  <th scope='col'>Status</th>
+                  <th scope='col'>Cadastrado em</th>
+                  <th scope='col'>#</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                  <td data-label="Cliente">Mecado esquina</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">Em aberto</td>
+                  <td data-label="Cadastrado">23/02/24</td>
+                  <td data-label="#">
+                    <button className='action' style={{ backgroundColor: '#3583f6' }}>
+                      <FiSearch color='#fff' size={17} />
+                    </button>
+                    <button className='action' style={{ backgroundColor: '#f6a935' }}> 
+                      <FiEdit2 color='#fff' size={17} />
+                    </button>
+                  </td>
+                </tr>
+            </tbody>
+
+         </table>
         </>
 
       </div>
